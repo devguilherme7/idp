7141b09ed9dcc5f1f0eba93666275ff96054dbb1
Guilherme Oliveira <oliveiraguilherme.pro@gmail.com>
2025-08-31 18:04:41 -0300

    Refactor project structure and update build configuration

--- prosallo-identity/src/main/java/org/prosallo/identity/ServerApplication.java
+++ prosallo-identity/src/main/java/org/prosallo/identity/ServerApplication.java
@@ -1,4 +1,4 @@
-package org.prosallo;
+package org.prosallo.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
-
 }
